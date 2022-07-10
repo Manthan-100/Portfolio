c30d28445a5694a90ee5327dd589abac263e6489
Manthan Shukla <manthanshukla@Manthans-MacBook-Air.local>
2022-07-10 18:50:47 +0530

    Updates

---
+++ precache-manifest.b1467c25e9618c22d91954106ac02712.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35ba5ce7f7579cf2dbb0d9276066014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fed58446b599f648034",
+    "url": "/static/css/main.a896dde0.chunk.css"
+  },
+  {
+    "revision": "9c98c212114292cd28c6",
+    "url": "/static/js/2.ff2ef139.chunk.js"
+  },
+  {
+    "revision": "a712f5023b98f1ee6f879156125341ec",
+    "url": "/static/js/2.ff2ef139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fed58446b599f648034",
+    "url": "/static/js/main.03431d63.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.03431d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "9388d6146b3f9ee4678dfadeddc22c51",
+    "url": "/static/media/8secondz.9388d614.png"
+  },
+  {
+    "revision": "c7b2dbe09bb2846e7a55189bf9e51233",
+    "url": "/static/media/bartini.c7b2dbe0.png"
+  },
+  {
+    "revision": "7feddacba0a519c07a32a37f3f27c9c9",
+    "url": "/static/media/cabbagetreelandscape.7feddacb.png"
+  },
+  {
+    "revision": "ad8ee816190aba472e8bb754fc080d11",
+    "url": "/static/media/cactus.ad8ee816.png"
+  },
+  {
+    "revision": "9ef3cb7262237d548954ce357584c36f",
+    "url": "/static/media/cactuscan.9ef3cb72.png"
+  },
+  {
+    "revision": "ad29052ffffcb6f4efa92c93a252ba88",
+    "url": "/static/media/manthan.ad29052f.jpeg"
+  },
+  {
+    "revision": "9c1952ca5232cf092e46425369fa3111",
+    "url": "/static/media/psykinetic.9c1952ca.png"
+  }
+]);

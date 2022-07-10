18adfc826b8cc5d14efb723df1b400da9cb067e8
Manthan Shukla <manthanshukla@Manthans-MacBook-Air.local>
2022-07-10 18:33:12 +0530

    fix changes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,17 +2,11 @@ import React from 'react'
 import './Footer.css'
 
 const Footer = () => {
-  return (
-    <footer>
-      <p className="copyright">
-        Made with{' '}
-        <span role="img" aria-label="thinking">
-          ❤️
-        </span>{' '}
-        by Matthew Jigalin
-      </p>
-    </footer>
-  )
+    return (
+        <footer>
+            <p className="copyright">Made by Manthan Shukla</p>
+        </footer>
+    )
 }
 
 export default Footer
